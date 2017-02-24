a0358dddbeee3c12eb39f492e275d055293fcb29
alu0100821390 <alu0100821390@ull.edu.es>
2017-02-24 13:58:05 +0000

    Añadir modificación

---
+++ Modification/README.md
@@ -0,0 +1,23 @@
+# Práctica 2: Cifrado de Vigenere (modificación)
+
+## Introducción
+
+Cifrado y descifrado de mensajes mediante el cifrado de Vigenere, incluyendo caracteres especiales al abecedario.
+
+## Ejecución
+
+~~~
+    py vigenere.py "'mensaje'" "'clave'"
+~~~
+
+Ejemplo de ejecución:
+
+~~~
+    py vigenere.py "este mensaje se autodestruira" "mision"
+~~~
+
+## Información personal
+
+**Autor:** [Kevin Estévez Expósito](https://alu0100821390.github.io/)
+
+**ULL-ID:** alu0100821390
---
+++ Modification/vigenere.py
@@ -0,0 +1,72 @@
+###################################################################################
+## Universidad de La Laguna						 								 ##
+## Escuela Superior de Ingeniería y Tecnología	 								 ##
+## Grado en Ingeniería Informática				 								 ##
+## Seguridad en Sistemas Informáticos			 								 ##
+## Fecha: 21/02/2016							 								 ##
+## Autor: Kevin Estévez Expósito (alu0100821390) 								 ##
+## 																				 ##
+## Práctica 2: Cifrado de Vigenere (CON MODIFICACIÓN)							 ##
+## Descripción: Cifrado y descifrado de mensajes mediante el cifrado Vigenere,	 ##
+## empleando el abecedario español en mayúscula sin 'CH', 'LL', 'Ñ', 'RR' ni 	 ##
+## espacios, pero con '*', '_', '?' y '¿'.										 ##
+##											 								 	 ##
+## Ejecución: py vigenere.py "'mensaje'" "'clave'"								 ##
+## Ejemplo de ejecución: py vigenere.py "este mensaje se autodestruira" "mision" ##
+###################################################################################
+
+
+import sys
+from operator import xor
+
+abecedario = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ*_?¿'
+mensaje = sys.argv[1].replace(' ', '').upper()	# Se guarda el mensaje pasado por parámetros, en mayúsculas y sin espacios #
+clave_original = sys.argv[2].replace(' ', '').upper()	# Se guarda la clave pasada por parámetros, en mayúsculas y sin espacios #
+clave = ''
+cifrado = ''
+descifrado = ''
+
+if len(mensaje)>len(clave_original):	# Si la logitud del mensaje es mayor que la de la clave... #
+	for i in range(int(len(mensaje)/len(clave_original))):		## Se alarga la clave, duplicándola y		 ##
+		clave += clave_original									## concatenándosela a sí misma, hasta que su ##
+	clave += clave_original[:len(mensaje)%len(clave_original)]	## longitud sea la misma que la del mensaje  ##
+
+elif len(mensaje)<len(clave_original):	# Si la longitud del mensaje es menor que la de la clave...
+	clave = clave_original[:len(mensaje)]	# Se trunca la clave para que tenga la misma longitud que el mensaje #
+
+elif len(mensaje)==len(clave_original):	# Si la longitud del mensaje es igual que la de la clave... #
+	clave = clave_original	# Se guarda la clave tal cual se encuentra en 'clave_original' #
+
+else:
+	print ('Ha ocurrido un error inesperado. Terminando ejecución...')
+	sys.exit(1)
+
+
+print ('Mensaje original: ' + mensaje)
+print ('Palabra clave: ' + clave_original)
+print ()
+
+
+print ('Cifrando...')
+for i in range(len(mensaje)):
+	x = abecedario.find(mensaje[i])	# Se guarda la posición del caracter del mensaje en el abecedario
+	y = abecedario.find(clave[i])	# Se guarda la posición del caracter de la clave en el abecedario
+	suma = x+y	# Se calcula la suma de ambas posiciones
+	modulo = suma%len(abecedario)	# Se calcula el módulo de la suma respecto a la longitud del abecedario
+	cifrado += abecedario[modulo]	# Se concatena el caracter cifrado con 'cifrado'
+
+print ('Mensaje cifrado: ' + cifrado)
+print ()
+	
+
+print ('Descifrando...')
+for i in range(len(cifrado)):
+	x = abecedario.find(cifrado[i])	# Se guarda la posición del caracter del mensaje cifrado en el abecedario
+	y = abecedario.find(clave[i])	# Se guarda la posición del caracter de la clave en el abecedario
+	resta = x-y	# Se calcula la resta de ambas posiciones
+	modulo = resta%len(abecedario)	# Se calcula el módulo de la resta respecto a la longitud del abecedario
+	descifrado += abecedario[modulo]	# Se concatena el caracter descifrado con 'descifrado'
+
+print ('Mensaje descifrado: ' + descifrado)
+
+sys.exit(0)
